7e7b2e3ce7f0258081f86ab39929629800f1e622
Rene90 <rob_rene199027@hotmail.com>
2023-03-06 17:27:23 -0700

    React hola mundo

---
+++ IntroReact/primera/src/App.js
@@ -0,0 +1,13 @@
+import HolaMundo from './components/HolaMundo.js';
+import './App.css';
+
+function App() {
+  return (
+    <>
+     <h1 className='texto-amarillo sombra'> Hola desde React App</h1>
+     <HolaMundo/> 
+    </>
+  );
+}
+
+export default App;
---
+++ IntroReact/primera/src/components/HolaMundo.js
@@ -0,0 +1,13 @@
+function HolaMundo(){
+    return(
+        <h2>Hola desde el componente Hola Mundo creado por Rene</h2>
+
+
+    )
+
+
+
+
+}
+
+export default  HolaMundo;
