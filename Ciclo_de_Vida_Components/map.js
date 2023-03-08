3acf5c6400964d22e10eb66e48f103bc6db9fe63
Rene90 <rob_rene199027@hotmail.com>
2023-03-08 16:21:02 -0700

    Ejemplo de map

---
+++ Ciclo_de_Vida_Components/map.js
@@ -0,0 +1,22 @@
+console.log("Este es un ejemplo de map")
+//listas o arrays que tiene precargados metodos y atributos
+
+const estudiantes = [
+	{nombre:"Rolando",mascota:"cuca"},
+	{nombre:"Rene",mascota:"Thor"},
+	{nombre:"Quidel",mascota:"Pandoro"},
+	{nombre:"Cristobal",mascota:"Firulais"}
+]
+//un atributo precargado seria length que nos da el tamanio de la lista
+
+console.log(`este es el tamanio de mi lista de estudiantes ${estudiantes.length}`)
+
+//metodo funcional for each, map , filter, reduce. El mas comun en React es map, porque nos recorre una lista entregandonos un arrelgo nuevo
+
+var estudiantesMascotas = estudiantes.map((elemento,indice,arreglo)=>{
+	return [elemento.nombre,elemento.mascota].join(" ")
+
+
+})
+
+console.log(`Este es mi nuevo arreglo ${estudiantesMascotas}`)
